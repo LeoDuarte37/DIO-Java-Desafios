b6d0a5f31d4d05d527482f275b1162234b3fb9ef
Leo Duarte <leonardo.r.s.duarte@gmail.com>
2024-09-24 23:19:11 -0300

    Fix

--- Patterns-Spring/src/main/java/com/leoDuarte37/Patterns_Spring/infrastructure/repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.leoDuarte37.Patterns_Spring.infrastructure.repository;
-
-import com.leoDuarte37.Patterns_Spring.infrastructure.model.Contact;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactRepository extends CrudRepository<Contact, String> {
-}
